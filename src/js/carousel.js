22ee347c6d3d79050ce01afbcf9b5176b03c22d5
Joriyx <jorgineoseland@gmail.com>
2026-05-16 22:31:10 +0200

    Update carousel.js
    Added function for constructing the carousel article

---
+++ src/js/carousel.js
@@ -0,0 +1,32 @@
+export function createCarousel(product) {
+  const article = document.createElement("article");
+  article.id = "carousel-article";
+
+  const a = document.createElement("a");
+  a.href = "/product/index.html?id=" + product.id;
+  article.appendChild(a);
+  const productImage = document.createElement("img");
+  productImage.className = "carousel-image";
+  productImage.src = product.image.url;
+  productImage.alt = product.image.alt;
+  a.appendChild(productImage);
+
+  const div = document.createElement("div");
+  article.appendChild(div);
+  const hgroup = document.createElement("hgroup");
+  div.appendChild(hgroup);
+  const h3 = document.createElement("h3");
+  h3.textContent = product.title;
+  hgroup.appendChild(h3);
+  const p = document.createElement("p");
+  p.textContent = product.price + "nok";
+  hgroup.appendChild(p);
+
+  const button = document.createElement("a");
+  button.className = "button";
+  button.href = "/product/index.html?id=" + product.id;
+  button.textContent = "Buy now";
+  div.appendChild(button);
+
+  return article;
+}
