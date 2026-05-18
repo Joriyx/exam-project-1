ca2537b62fd2cb33dfe403495cab4a6681e3c298
Joriyx <jorgineoseland@gmail.com>
2026-05-18 14:48:19 +0200

    Product details fetch
    Added logic for getting the product details from the api

---
+++ product/productPage.js
@@ -0,0 +1,30 @@
+import { getProduct } from "../src/js/api.js";
+import { createProductDetails } from "../src/js/product.js";
+
+async function refreshProductPage() {
+  const productDetail = document.getElementById("product-details");
+  if (!productDetail) {
+    return;
+  }
+  productDetail.innerHTML = "";
+  const query = new URLSearchParams(window.location.search);
+  const id = query.get("id");
+  if (!id) {
+    const msg = document.createElement("h1");
+    msg.textContent = "Product not found";
+    productDetail.appendChild(msg);
+    return;
+  }
+  const product = await getProduct(id);
+  if (!product) {
+    const msg = document.createElement("h1");
+    msg.textContent = "Product not found";
+    productDetail.appendChild(msg);
+    return;
+  }
+
+  const productSections = createProductDetails(product);
+  productDetail.append(...productSections);
+}
+
+document.onreadystatechange = refreshProductPage;
