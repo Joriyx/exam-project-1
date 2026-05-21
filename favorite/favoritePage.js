17b68caa61d69fc8785e550769975de3fca049d6
Joriyx <jorgineoseland@gmail.com>
2026-05-21 20:37:14 +0200

    Add all to cart
    Added function for adding all products in the wishlist to the cart

--- favorite/favoritePage.js
+++ favorite/favoritePage.js
@@ -7,27 +7,38 @@ import {
   refreshFavoritesTotal,
 } from "../src/js/favorite.js";
 import { getFavorites } from "../src/js/favorite.js";
+import { addToCart } from "../src/js/cart.js";
 
 async function refreshFavoritePage() {
   refreshProfileHeader();
   refreshFavoritesTotal();
   const favoritesList = document.getElementById("favorite-list");
-  if (!favoritesList) {
-    return;
-  }
-  favoritesList.innerHTML = "";
-  const products = getFavorites();
-  if (products.length === 0) {
-    const noItems = document.createElement("article");
-    favoritesList.appendChild(noItems);
-    const noItemsH2 = document.createElement("h2");
-    noItemsH2.textContent = "No Products in wishlist";
-    noItems.appendChild(noItemsH2);
-    return;
+  if (favoritesList) {
+    favoritesList.innerHTML = "";
+    const products = getFavorites();
+    if (products.length === 0) {
+      const noItems = document.createElement("article");
+      favoritesList.appendChild(noItems);
+      const noItemsH2 = document.createElement("h2");
+      noItemsH2.textContent = "No Products in wishlist";
+      noItems.appendChild(noItemsH2);
+      return;
+    }
+
+    for (let i = 0; i < products.length; i++) {
+      favoritesList.appendChild(createFavoriteCard(products[i]));
+    }
   }
 
-  for (let i = 0; i < products.length; i++) {
-    favoritesList.appendChild(createFavoriteCard(products[i]));
+  const addAllButton = document.getElementById("all-to-cart");
+  if (addAllButton) {
+    function onAddAllClicked() {
+      const favorites = getFavorites();
+      for (let i = 0; i < favorites.length; i++) {
+        addToCart(favorites[i]);
+      }
+    }
+    addAllButton.onclick = onAddAllClicked;
   }
 }
 
