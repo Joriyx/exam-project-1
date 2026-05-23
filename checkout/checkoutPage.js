4a169950a42ac7c32649b9da021476c71356ac66
Joriyx <jorgineoseland@gmail.com>
2026-05-23 21:47:21 +0200

    Check out button
    Added functionality to take the user from the checkout page to the success page

--- checkout/checkoutPage.js
+++ checkout/checkoutPage.js
@@ -1,5 +1,5 @@
 import { refreshProfileHeader } from "../account/account.js";
-import { refreshCartCounter } from "../src/js/cart.js";
+import { refreshCartCounter, clearCart } from "../src/js/cart.js";
 import { refreshFavoritesCounter } from "../src/js/favorite.js";
 
 refreshCartCounter();
@@ -7,6 +7,16 @@ refreshFavoritesCounter();
 
 async function refreshCheckoutPage() {
   refreshProfileHeader();
+
+  const clearButton = document.getElementById("checkout-button");
+  if (clearButton) {
+    function onClearClick(event) {
+      event.preventDefault();
+      clearCart();
+      window.location.replace("/success/index.html");
+    }
+    clearButton.onclick = onClearClick;
+  }
 }
 
 document.onreadystatechange = function () {
--- success/successPage.js
+++ success/successPage.js
@@ -1,6 +1,8 @@
 import { refreshProfileHeader } from "../account/account.js";
 import { refreshFavoritesCounter } from "../src/js/favorite.js";
+import { refreshCartCounter } from "../src/js/cart.js";
 
+refreshCartCounter();
 refreshFavoritesCounter();
 
 async function refreshSuccessPage() {
