0b3cc56fd859c54668637b397803a0db9181c56b
Joriyx <jorgineoseland@gmail.com>
2026-05-23 20:52:32 +0200

    Update successPage.js
    added refresh function for success page

--- success/successPage.js
+++ success/successPage.js
@@ -2,3 +2,13 @@ import { refreshProfileHeader } from "../account/account.js";
 import { refreshFavoritesCounter } from "../src/js/favorite.js";
 
 refreshFavoritesCounter();
+
+async function refreshSuccessPage() {
+  refreshProfileHeader();
+}
+
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    refreshSuccessPage();
+  }
+};
